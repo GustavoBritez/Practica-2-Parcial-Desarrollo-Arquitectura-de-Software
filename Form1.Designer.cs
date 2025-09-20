80309fa7c30352b86531388b7e119299cac31530
Britez Gustavo <danielgus@outlook.es>
2025-09-19 22:51:51 -0300

    Modelo parcial 2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            Lista_Tablas = new ListBox();
+            Grilla_Estudiantes = new DataGridView();
+            Grilla_Reserva = new DataGridView();
+            Grilla_Libros = new DataGridView();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            BTN_AGREGAR = new Button();
+            BTN_ASIGNAR = new Button();
+            textBox1 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)Grilla_Estudiantes).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Grilla_Reserva).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Grilla_Libros).BeginInit();
+            SuspendLayout();
+            // 
+            // Lista_Tablas
+            // 
+            Lista_Tablas.FormattingEnabled = true;
+            Lista_Tablas.Location = new Point(617, 232);
+            Lista_Tablas.Name = "Lista_Tablas";
+            Lista_Tablas.Size = new Size(138, 94);
+            Lista_Tablas.TabIndex = 0;
+            // 
+            // Grilla_Estudiantes
+            // 
+            Grilla_Estudiantes.AllowUserToAddRows = false;
+            Grilla_Estudiantes.AllowUserToDeleteRows = false;
+            Grilla_Estudiantes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            Grilla_Estudiantes.Location = new Point(22, 21);
+            Grilla_Estudiantes.Name = "Grilla_Estudiantes";
+            Grilla_Estudiantes.ReadOnly = true;
+            Grilla_Estudiantes.Size = new Size(240, 150);
+            Grilla_Estudiantes.TabIndex = 2;
+            Grilla_Estudiantes.RowEnter += Grilla_Estudiantes_RowEnter;
+            Grilla_Estudiantes.SelectionChanged += Grilla_Estudiantes_SelectionChanged;
+            // 
+            // Grilla_Reserva
+            // 
+            Grilla_Reserva.AllowUserToAddRows = false;
+            Grilla_Reserva.AllowUserToDeleteRows = false;
+            Grilla_Reserva.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            Grilla_Reserva.Location = new Point(22, 232);
+            Grilla_Reserva.Name = "Grilla_Reserva";
+            Grilla_Reserva.ReadOnly = true;
+            Grilla_Reserva.Size = new Size(471, 150);
+            Grilla_Reserva.TabIndex = 3;
+            // 
+            // Grilla_Libros
+            // 
+            Grilla_Libros.AllowUserToAddRows = false;
+            Grilla_Libros.AllowUserToDeleteRows = false;
+            Grilla_Libros.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            Grilla_Libros.Location = new Point(399, 21);
+            Grilla_Libros.Name = "Grilla_Libros";
+            Grilla_Libros.ReadOnly = true;
+            Grilla_Libros.Size = new Size(240, 150);
+            Grilla_Libros.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(504, 3);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Libros";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(102, 214);
+            label3.Name = "label3";
+            label3.Size = new Size(52, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Reservas";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(102, 3);
+            label4.Name = "label4";
+            label4.Size = new Size(62, 15);
+            label4.TabIndex = 7;
+            label4.Text = "Estudiante";
+            // 
+            // BTN_AGREGAR
+            // 
+            BTN_AGREGAR.Location = new Point(22, 177);
+            BTN_AGREGAR.Name = "BTN_AGREGAR";
+            BTN_AGREGAR.Size = new Size(75, 23);
+            BTN_AGREGAR.TabIndex = 8;
+            BTN_AGREGAR.Text = "Agregar";
+            BTN_AGREGAR.UseVisualStyleBackColor = true;
+            BTN_AGREGAR.Click += BTN_AGREGAR_Click;
+            // 
+            // BTN_ASIGNAR
+            // 
+            BTN_ASIGNAR.Location = new Point(292, 86);
+            BTN_ASIGNAR.Name = "BTN_ASIGNAR";
+            BTN_ASIGNAR.Size = new Size(75, 23);
+            BTN_ASIGNAR.TabIndex = 9;
+            BTN_ASIGNAR.Text = "Asignar";
+            BTN_ASIGNAR.UseVisualStyleBackColor = true;
+            BTN_ASIGNAR.Click += BTN_ASIGNAR_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(635, 332);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 10;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(796, 394);
+            Controls.Add(textBox1);
+            Controls.Add(BTN_ASIGNAR);
+            Controls.Add(BTN_AGREGAR);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(Grilla_Libros);
+            Controls.Add(Grilla_Reserva);
+            Controls.Add(Grilla_Estudiantes);
+            Controls.Add(Lista_Tablas);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)Grilla_Estudiantes).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Grilla_Reserva).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Grilla_Libros).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListBox Lista_Tablas;
+        private DataGridView Grilla_Estudiantes;
+        private DataGridView Grilla_Reserva;
+        private DataGridView Grilla_Libros;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button BTN_AGREGAR;
+        private Button BTN_ASIGNAR;
+        private TextBox textBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,174 @@
+using Microsoft.VisualBasic;
+using System.Data;
+
 namespace Biblioteca
 {
     public partial class Form1 : Form
     {
+        DataSet ds = new DataSet();
+        BindingSource bdEstudiante = new BindingSource();
+        BindingSource bdLibros = new BindingSource();
+        BindingSource bdReservas = new BindingSource();
+
         public Form1()
         {
             InitializeComponent();
+            Cargar_DataSet("D:\\Documents\\BASE_DAS\\LOPO.xml");
+            Cargar_Grillas();
         }
+        public void Cargar_DataSet(string linea)
+        {
+            try
+            {
+
+                ds.ReadXml(linea);
+
+                DataRelation relation_Estudiante_Prestamo = new DataRelation(
+                    "FK_EST_PRESTAMO",
+                    ds.Tables["Estudiante"].Columns["Legajo"],
+                    ds.Tables["Prestamo"].Columns["Legajo"]
+                    );
+
+                DataRelation relation_Libro_Prestamo = new DataRelation(
+                    "FK_LIBRO_PRESTAMO",
+                    ds.Tables["Libro"].Columns["ID"],
+                    ds.Tables["Prestamo"].Columns["ID"]
+                    );
+
+                ds.Relations.Add(relation_Estudiante_Prestamo);
+                ds.Relations.Add(relation_Libro_Prestamo);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void Cargar_Grillas()
+        {
+            try
+            {
+
+                bdEstudiante.DataSource = ds;
+                bdEstudiante.DataMember = "Estudiante";
+
+                Grilla_Estudiantes.DataSource = bdEstudiante;
+
+                bdLibros.DataSource = ds;
+                bdLibros.DataMember = "Libro";
+
+                Grilla_Libros.DataSource = bdLibros;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Grilla_Estudiantes_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (e.RowIndex >= 0)
+                {
+                    var inn = Grilla_Estudiantes.Rows[e.RowIndex].Cells["Legajo"].Value;
+                    DataView dv = new DataView(ds.Tables["Prestamo"]);
+                    dv.RowFilter = $"Legajo = {inn}";
+                    Grilla_Reserva.DataSource = dv;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void Grilla_Estudiantes_SelectionChanged(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void BTN_AGREGAR_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string nombre = Interaction.InputBox("Ingrese nombre");
+                int legajo = Convert.ToInt32(Interaction.InputBox("Ingrese legajo numerico"));
+
+                ds.Tables["Estudiante"].Rows.Add(legajo, nombre);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                ds.WriteXml("D:\\Documents\\BASE_DAS\\LOPO.xml");
+            }
+        }
+
+        private void BTN_ASIGNAR_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int legajo = Convert.ToInt32(Grilla_Estudiantes.SelectedRows[0].Cells["Legajo"].Value);
+                int id = Convert.ToInt32(Grilla_Libros.SelectedRows[0].Cells["ID"].Value);
+
+
+                ds.Tables["Prestamo"].Rows.Add(legajo, id, "Pendiente");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                ds.WriteXml("D:\\Documents\\BASE_DAS\\LOPO.xml");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string text = textBox1.Text.Trim();
+
+                Lista_Tablas.DataSource = null;
+
+                DataView dv = new DataView(ds.Tables["Libro"]);
+
+                if (!string.IsNullOrEmpty(text))
+                {
+                    dv.RowFilter = $"Titulo LIKE '%{text}%'";
+                }
+                else
+                {
+                    dv.RowFilter = "";
+                }
+
+                Lista_Tablas.DataSource = dv;
+                Lista_Tablas.DisplayMember = "Titulo";  
+                Lista_Tablas.ValueMember = "ID";         
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                ds.WriteXml("D:\\Documents\\BASE_DAS\\LOPO.xml");
+            }
+        }
+
     }
 }
